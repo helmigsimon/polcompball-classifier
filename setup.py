7dd3537e790185380342d3c719ee98761d13d6a9
Simon Helmig <helmigsimon@gmail.com>
2020-10-22 15:39:28 +0200

    Initial commit post-cookiecutter setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Multi-label classification computer vision model trained on images obtained from the r/PolCompBall subreddit. ',
+    author='Simon Helmig',
+    license='MIT',
+)
